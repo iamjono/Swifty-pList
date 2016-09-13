9a0655e893083ac74d17d39251a2d59f787aca24
Jonathan Guthrie <jono@perfect.org>
2016-09-13 10:02:27 -0400

    Initial commit

---
+++ ApplicationConfiguration.plist.cpy
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+	<dict>
+		<key>postgresdbname</key>
+		<string>[dbname]</string>
+		<key>postgreshost</key>
+		<string>localhost</string>
+		<key>postgresport</key>
+		<string>5432</string>
+		<key>postgrespwd</key>
+		<string>[pwd]</string>
+		<key>postgresuser</key>
+		<string>[username]</string>
+		<key>clientid</key>
+		<string>[your id here]</string>
+		<key>clientsecret</key>
+		<string>[your secret here]</string>
+	</dict>
+</plist>
---
+++ Sources/plist.swift
@@ -0,0 +1,114 @@
+//
+//  Plist.swift
+//
+//  Created by Jonathan Guthrie on 2016-08-24.
+//
+//
+
+/*
+Example Usage:
+
+func GetAPIKey() -> AppCredentials {
+	var creds = AppCredentials()
+	if let plist = Plist(name: "ApplicationConfiguration") {
+
+		let dict = plist.getMutablePlistFile()!
+
+		creds.clientid = dict["clientid"] as! String
+		creds.clientsecret = dict["clientid"] as! String
+
+		if creds.clientid.characters.count > 0 {
+			return creds
+		} else {
+			print("No API Key")
+		}
+	} else {
+		print("Unable to get Plist")
+	}
+	return creds
+}
+
+func GetAPIToken() -> String {
+	var token = ""
+	if let plist = Plist(name: "ApplicationConfiguration") {
+
+		let dict = plist.getMutablePlistFile()!
+
+		token = dict["token"] as! String
+
+		if token.characters.count > 0 {
+			return token
+		} else {
+			print("No API Key")
+		}
+	} else {
+		print("Unable to get Plist")
+	}
+	return token
+}
+
+*/
+
+
+
+import Foundation
+import PerfectLib
+
+struct Plist {
+
+	enum PlistError: Error {
+		case FileNotWritten
+		case FileDoesNotExist
+	}
+
+	let name:String
+
+	init?(name:String) {
+
+		self.name = name
+
+		let thisFile = File("\(name).plist")
+		if thisFile.exists == false {
+			print("Settings file does not exist")
+		}
+	}
+
+	func getValuesInPlistFile() -> NSDictionary?{
+		let thisFile = File("\(name).plist")
+
+		if thisFile.exists {
+			guard let dict = NSDictionary(contentsOfFile: thisFile.realPath) else {
+				return .none
+			}
+			return dict
+		} else {
+			return .none
+		}
+	}
+
+	func getMutablePlistFile() -> NSMutableDictionary?{
+		let thisFile = File("\(name).plist")
+
+		if thisFile.exists {
+			guard let dict = NSMutableDictionary(contentsOfFile: thisFile.realPath) else {
+				return .none
+			}
+			return dict
+		} else {
+			return .none
+		}
+	}
+
+	func addValuesToPlistFile(dictionary:NSDictionary) throws {
+		let thisFile = File("\(name).plist")
+
+		if thisFile.exists {
+			if !dictionary.write(toFile: thisFile.realPath, atomically: false) {
+				print("File not written successfully")
+				throw PlistError.FileNotWritten
+			}
+		} else {
+			throw PlistError.FileDoesNotExist
+		}
+	}
+}
